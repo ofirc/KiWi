b4839b035c3cc1f632407646e79d5fe68f4e32b0
Ofir Cohen <ofir.o.cohen@intel.com>
2019-02-11 19:46:57 +0200

    Implemented test

--- src/main/java/kiwi/KiWiMap.java
+++ src/main/java/kiwi/KiWiMap.java
@@ -1,5 +1,14 @@
 package kiwi;
 
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.Random;
+
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
@@ -38,11 +47,11 @@ public class KiWiMap implements CompositionalMap<Integer,Integer>
     }
     
     /** requires full scan() for atomic size() */
-    @Override
-    public int size()
-    {
-        return -1;
-    }
+//    @Override
+//    public int size()
+//    {
+//        return -1;
+//    }
     
     /** not implemented ATM - can be implemented with chunk.findFirst() */
     @Override
@@ -61,6 +70,8 @@ public class KiWiMap implements CompositionalMap<Integer,Integer>
     public Integer put(Integer k, Integer v)
     {
     	kiwi.put(k, v);
+        size.incrementAndGet();
+        sum.getAndAdd(k);
         return null;
     }
 
@@ -69,6 +80,8 @@ public class KiWiMap implements CompositionalMap<Integer,Integer>
     public Integer remove(Object o)
     {
     	kiwi.put((Integer)o, null);
+        size.decrementAndGet();
+        sum.getAndAdd(-(Integer)o);
         return null;
     }
 
@@ -166,5 +179,147 @@ public class KiWiMap implements CompositionalMap<Integer,Integer>
     public void calcChunkStatistics()
     {
         kiwi.calcChunkStatistics();
+    }
+
+    private static AtomicLong totalKeys = new AtomicLong(0);
+    private static AtomicLong totalNodes = new AtomicLong(0);
+    private static int numI;
+    private static int numR;
+
+    private AtomicInteger size = new AtomicInteger();
+    private AtomicLong sum = new AtomicLong();
+
+    @Override
+    public int size()
+    {
+        return size.get();
+    }
+
+    private long getKeysum()
+    {
+        return sum.get();
+    }
+
+    private boolean contains(int key)
+    {
+        Integer val = get(key);
+        return (val != null && val == 0);
+    }
+
+    public static void main2(String[] args) throws InterruptedException
+    {
+        KiWiMap kiwi = new KiWiMap();
+        int key = 30;
+        kiwi.put(key, 0);
+        totalKeys.addAndGet(key);
+        totalNodes.incrementAndGet();
+
+        assert totalKeys.get() == kiwi.getKeysum();
+        assert totalNodes.get() == kiwi.size();
+
+        key = 90;
+        kiwi.put(key, 0);
+        totalKeys.addAndGet(key);
+        totalNodes.incrementAndGet();
+
+        assert totalKeys.get() == kiwi.getKeysum();
+        assert totalNodes.get() == kiwi.size();
+
+        kiwi.remove(key);
+        totalKeys.addAndGet(-key);
+        totalNodes.decrementAndGet();
+
+        assert totalKeys.get() == kiwi.getKeysum();
+        assert totalNodes.get() == kiwi.size();
+
+        System.out.printf("%d %d%n", totalKeys.get(), kiwi.getKeysum());
+        System.out.printf("%d %d%n", totalNodes.get(), kiwi.size());
+    }
+
+    private static final int num_iters = 1000000;
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        KiWiMap kiwi = new KiWiMap();
+
+        ConcurrentMap concurrentMap = new ConcurrentHashMap<Integer, Integer>();
+
+        Random random = new Random();
+        final int numThreads = 8;
+        ThreadPoolExecutor executor =
+                (ThreadPoolExecutor) Executors.newFixedThreadPool(numThreads);
+
+        for (int cur_iter = 0; cur_iter < num_iters; cur_iter++) {
+//            if (cur_iter % (num_iters / 100) == 0) {
+//                System.out.println(cur_iter / (num_iters / 100));
+//            }
+            int randonOp = random.nextInt(2);
+            if (randonOp == 0) {
+                final int key = random.nextInt(50000);
+                executor.submit(() -> kiwi.put(key, 0));
+                totalKeys.addAndGet(key);
+                totalNodes.incrementAndGet();
+                numI++;
+                concurrentMap.put(key, 0);                
+            } else {
+                final int key = random.nextInt(50000);
+                numR++;
+                executor.submit(() -> {
+                    if (concurrentMap.remove(key) != null) {
+                        totalKeys.addAndGet(-key);
+                        totalNodes.decrementAndGet();
+                        kiwi.remove(key);
+                    }
+                });
+//                boolean res = concurrentMap.remove((Integer)key);
+//                if (res)
+//                    executor.submit(() -> kiwi.remove(key));
+            }
+        }
+
+        for (int cur_iter = 0; cur_iter < num_iters; cur_iter++) {
+//            if (cur_iter % (num_iters / 100) == 0) {
+//                System.out.println(cur_iter / (num_iters / 100));
+//            }
+            int randonOp = random.nextInt(3);
+            if (randonOp == 0) {
+                final int key = random.nextInt(50000);
+                concurrentMap.put(key, 0);                
+                totalKeys.addAndGet(key);
+                totalNodes.incrementAndGet();
+                numI++;
+                executor.submit(() -> kiwi.put(key, 0));
+            } else if(randonOp == 1){
+                final int key = random.nextInt(50000);
+                numR++;
+                Integer val = (Integer)concurrentMap.remove(key);
+                executor.submit(() -> {
+                    if (concurrentMap.remove(key) != null) {
+                        totalKeys.addAndGet(-key);
+                        totalNodes.decrementAndGet();
+                        kiwi.remove(key);
+                    }
+                });
+//                boolean res = concurrentMap.remove(key);
+//                if (res)
+//                    executor.submit(() -> kiwi.remove(key));
+            } else {
+                final int key = random.nextInt(50000);
+                executor.submit(() -> {
+//                    if (kiwi.contains(key))
+//                        System.out.println("kiwi contains" + key);
+                  }
+                );
+            }
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.DAYS);
+//        System.out.printf("%d %d%n", numR, numI);
+        assert totalKeys.get() == kiwi.getKeysum();
+        assert totalNodes.get() == kiwi.size();
+        System.out.printf("%d %d%n", totalKeys.get(), kiwi.getKeysum());
+        System.out.printf("%d %d%n", totalNodes.get(), kiwi.size());
+
     }
 }
